7984847fa75db609dee615c674745764ea3a32a4
Eryka Shi-Shun <31933725+shishune@users.noreply.github.com>
2024-06-21 16:16:33 -0400

    alllow requests from non authenticated users

--- backend/clubclubgobackend/clubclubgo/views.py
+++ backend/clubclubgobackend/clubclubgo/views.py
@@ -1,6 +1,7 @@
 from clubclubgo.models import Club, Event
 from rest_framework import permissions, viewsets
 
+
 from clubclubgo.serializers import ClubSerializer, EventSerializer
 
 
@@ -10,7 +11,20 @@ class ClubViewSet(viewsets.ModelViewSet):
     """
     queryset = Club.objects.all()
     serializer_class = ClubSerializer
-    # permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated]
+    permission_classes_by_action = {'list': [permissions.AllowAny],
+                                    'retrieve': [permissions.AllowAny]}
+    
+    def get_permissions(self):
+        try:
+            # return permission_classes depending on `action` 
+            return [permission() for permission in self.permission_classes_by_action[self.action]]
+        except KeyError: 
+            # action is not set return default permission_classes
+            return [permission() for permission in self.permission_classes]
+    
+
+
 
 
 class EventViewSet(viewsets.ModelViewSet):
@@ -19,5 +33,15 @@ class EventViewSet(viewsets.ModelViewSet):
     """
     queryset = Event.objects.all().order_by('date_time')
     serializer_class = EventSerializer
-    # permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated]
+    permission_classes_by_action = {'list': [permissions.AllowAny],
+                                    'retrieve': [permissions.AllowAny]}
+    
+    def get_permissions(self):
+        try:
+            # return permission_classes depending on `action` 
+            return [permission() for permission in self.permission_classes_by_action[self.action]]
+        except KeyError: 
+            # action is not set return default permission_classes
+            return [permission() for permission in self.permission_classes]
 
