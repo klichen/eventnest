bf3e7f60fd6b0dd0e9b9b04bec3cffe7158a25b2
Eryka Shi-Shun <31933725+shishune@users.noreply.github.com>
2024-07-19 16:39:59 -0400

    add search/filter get method to events that filters date, title, descrition and club names

--- backend/clubclubgobackend/urls.py
+++ backend/clubclubgobackend/urls.py
@@ -29,4 +29,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('upcoming/', views.getUpcomingEvents),
     path('club/<id>/events/', views.getClubEvents),
+    path('search/', views.searchEvents),
 ]
